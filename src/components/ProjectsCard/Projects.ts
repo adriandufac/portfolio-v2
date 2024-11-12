5b5be8bde5e3c5633e90d644a37d194956f8dbcd
Nerz <adrian.dufac@gmail.com>
2024-11-12 17:15:58 +0100

    update projects section

--- src/components/ProjectsCard/Projects.ts
+++ src/components/ProjectsCard/Projects.ts
@@ -18,6 +18,19 @@ const projects = [
   },
   {
     id: 2,
+    title: "Prism Tales",
+    description:
+      "This is a portfolio that I've made for a photographer. I connected it to a back-office using sanity.io so the admin can easyly add content.",
+    links: [
+      {
+        name: "View Site",
+        href: "https://portfolio-example-smoky.vercel.app/",
+      },
+    ],
+    tags: ["React", "Framer motion", "Tailwind", "Sanity"],
+  },
+  {
+    id: 3,
     title: "Game Hub",
     description:
       "GameHub listing all sort of games that are fetched using rawg.io API.\n The games can be sorted by all platform or/and genre.",
@@ -28,7 +41,7 @@ const projects = [
     tags: ["React", "HTML", "CSS", "API", "Axios"],
   },
   {
-    id: 3,
+    id: 4,
     title: "Club M'aide",
     description:
       "School work I did at ENI school, this is a hub for company where differents users can create outdoor evenement with localisation and other users can notify if they want to participate. You can connect using username : recherche@alternance.fr and pw: recherche",
@@ -37,14 +50,6 @@ const projects = [
       { name: "GitHub", href: "https://github.com/adriandufac/ClubMAide" },
     ],
     tags: ["PHP", "Symfony"],
-  },
-  {
-    id: 4,
-    title: "TFT data Analyses",
-    description:
-      "This is a project I made using riot API to collect data about thousands TFT games played by the best players. These datas are stored in a database using mySql. Then I use DBScan algorithm to cluster the different composition. Finaly I made an algorithm to extract statistics about all thoses compositions and store it in the database.",
-    links: [{ name: "GitHub", href: "https://github.com/adriandufac/TFT_V2" }],
-    tags: ["Java", "API", "SQL"],
   },
 ];
 
